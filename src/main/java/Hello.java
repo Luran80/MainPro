9a9e492796f121057050c94f7c24df09cf639164
Gizedor <Gizedor90@gmail.com>
2018-04-16 21:11:08 +0300

    gav gav

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,24 @@
 package main.java;
 
+
+import java.util.ArrayList;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        ArrayList gav = new ArrayList<String>();
+        gav.add("1 dog");
+        gav.add("2 dog");
+        gav.add("3 dog");
+        gav.add("4 dog");
+        gav.add("5 dog");
+        gav.add("6 dog");
+
+        System.out.println(gav.get(0));
     }
+    public ArrayList<String> doge(ArrayList<String> gav){
+        return gav;
+    }
+
+
 }
